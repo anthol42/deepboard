581056c946152451eabf7fa8e556d23e9453c724
Anthony Lavertu <alavertu2@gmail.com>
2025-05-05 11:00:26 -0400

    Feat(initial commit) + Feat(resultTable): Created the result table code + docstring

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import os
+
+if os.path.exists("README.md"):
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+else:
+    long_description = "No description found"
+
+# Load version
+with open("deepboard/__version__.py", "r") as f:
+    version = f.read().split("=")[1].strip().strip("\"")
+
+setup(
+    name="deepboard",
+    version=version,
+    author="Anthony Lavertu",
+    author_email="alavertu2@gmail.com",
+    include_package_data=True,
+    description="A tool to log you experiment results and explore them in a gui",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anthol42/deepboard",
+    project_urls={
+        "Issues": "https://github.com/anthol42/deepboard/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    keywords=[
+        "deepboard", "deep", "board", "pytorch", "torch", "tensorflow", "jax", "tensorboard"
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "PyYAML>=6.0.0",
+    ],
+)
